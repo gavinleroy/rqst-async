0d7331153dcdc071847661437916cb8da8f50ed1
gavinleroy <gavinleroy6@gmail.com>
2025-05-05 12:48:31 -0400

    Closes #7

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,6 +1,7 @@
 use miniserve::{http::StatusCode, Content, Request, Response};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
+use tokio::join;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 struct Data {
@@ -15,7 +16,12 @@ async fn chat(req: Request) -> Response {
     };
 
     let mut data: Data = serde_json::from_str(&str).map_err(|_| StatusCode::BAD_REQUEST)?;
-    data.messages.push("Howdy!".to_string());
+
+    let messages = chatbot::query_chat(&data.messages);
+    let idx = chatbot::gen_random_number();
+    let (mut responses, idx) = join!(messages, idx);
+    let idx = idx % responses.len();
+    data.messages.push(std::mem::take(&mut responses[idx]));
     let resp = json!(data).to_string();
     Response::Ok(Content::Json(resp))
 }
